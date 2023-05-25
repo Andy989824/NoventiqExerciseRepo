397bda137b472f68571823e94d519a07bb2f06a8
Andy989824 <andylau989824@gmail.com>
2023-05-25 15:37:51 +0800

    edit parse string in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = "#serverPort#";
+const port = parseInt("#serverPort#");
 
 const corsOptions = {
   origin: ["#frontendAPI#"],
