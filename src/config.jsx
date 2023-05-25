67a7adeb1c7eb54d79c0210045f84db573b1ba53
Andy989824 <andylau989824@gmail.com>
2023-05-25 08:43:21 +0800

    test replace token on dbURL and add test file

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from "firebase/database";
 
-const dbURL = process.env.DB_CONNECTION_URL
 
 const firebaseConfig = {
 
@@ -11,8 +10,8 @@ const firebaseConfig = {
     storageBucket: "noventiq-exercise-db.appspot.com",
     messagingSenderId: "391159275978",
     appId: "1:391159275978:web:f057e4c7968cb6d995ef33",
-    //databaseURL: dbURL
-    databaseURL: "https://noventiq-exercise-db-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "#dbURL#",
+    //databaseURL: "https://noventiq-exercise-db-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
 };
 
