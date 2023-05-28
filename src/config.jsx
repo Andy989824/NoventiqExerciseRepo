551ab427ce4caf8041aec4ab7421fa01cc010f83
Andy989824 <andylau989824@gmail.com>
2023-05-28 17:35:20 +0800

    update db variable

--- src/config.jsx
+++ src/config.jsx
@@ -11,7 +11,8 @@ const firebaseConfig = {
     appId: "1:391159275978:web:f057e4c7968cb6d995ef33",
     //databaseURL: "#dbURL#",
     //databaseURL: "https://noventiq-exercise-db-default-rtdb.asia-southeast1.firebasedatabase.app/"
-    databaseURL: `${process.env.REACT_APP_DBURL}`
+    //databaseURL: `${process.env.REACT_APP_DBURL}`
+    databaseURL: "#{REACT_APP_DB}#"
 };
 
 const app = initializeApp(firebaseConfig);
